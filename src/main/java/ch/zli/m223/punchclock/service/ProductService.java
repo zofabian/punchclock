55957be267ac0fab96eaa8d38cb798b9e85afafd
fabzol <Ruggenacher2>
2020-07-10 13:50:36 +0200

    ProductService Implemented

--- src/main/java/ch/zli/m223/punchclock/service/ProductService.java
+++ src/main/java/ch/zli/m223/punchclock/service/ProductService.java
@@ -1,4 +1,28 @@
 package ch.zli.m223.punchclock.service;
 
+import ch.zli.m223.punchclock.domain.Product;
+import ch.zli.m223.punchclock.repository.productRepository;
+
+import java.util.List;
+
 public class ProductService {
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Entry entry) {
+        return productRepository.saveAndFlush(entry);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public void deleteProduct(long id){productRepository.deleteById(id);}
+
+    public void  updateProduct(Entry entry) {
+        productRepository.save(entry);
+    }
 }
